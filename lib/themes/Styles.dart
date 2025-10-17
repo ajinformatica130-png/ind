e68da1e1b47011eaf6755b75ad502f956e1017e3
ajinformatica130-png <ajinformatica130@gmail.com>
2025-10-17 08:20:02 -0300

    Update import path for app_colors.dart

--- lib/themes/Styles.dart
+++ lib/themes/Styles.dart
@@ -1,4 +1,4 @@
-import 'package:customer/themes/app_colors.dart';
+import 'package:tochegandodelivery/themes/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
